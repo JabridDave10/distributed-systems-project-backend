2abb5f2a382de3d9e1f4b876f46259bf4f199c5d
JuanJimenezHOC <djimenez@hoc.com.co>
2025-09-28 20:41:48 -0500

    Se actualizó la configuración de CORS para permitir orígenes específicos en lugar de permitir todos temporalmente, mejorando la seguridad de la aplicación.

--- app/core/middleware.py
+++ app/core/middleware.py
@@ -21,7 +21,7 @@ def configure_middleware(app):
 
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["*"],  # Temporalmente permitir todos los origins
+        allow_origins=origins,
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
